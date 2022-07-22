1b689108e44fe89534ce2a784d2082cde94b263e
Andrew Kain <hello@andreww.io>
2022-07-22 19:40:08 +0400

    functional on the deprecated version of the API

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,8 +1,17 @@
 import GoogleMapReact from 'google-map-react'
 import LocationMarker from './LocationMarker'
 
+const NATURAL_EVENT_WILDFIRE = 8;
 
-const Map = ({ center, zoom }) => {
+const Map = ({ eventData, center, zoom }) => {
+  const markers = eventData.map((ev) => {
+    if(ev.categories[0].id === NATURAL_EVENT_WILDFIRE) {
+        return <LocationMarker lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]} />
+    }
+    return null
+})
+  
+
   return (
     <div className="map">
       <GoogleMapReact
@@ -10,7 +19,8 @@ const Map = ({ center, zoom }) => {
         defaultCenter={ center }
         defaultZoom={ zoom }
       >
-        <LocationMarker lat={center.lat} lng={center.lng} />
+<LocationMarker lat={42} lng={-122} />
+{markers}
       </GoogleMapReact>
     </div>
   )
